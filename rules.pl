24ba5a11e207881c69549420955c510c5b38173e
Bengt Thuree <bthuree@linuxfoundation.org>
2020-06-16 16:32:57 +1000

    First version of Prolog filter for INFO.yaml
    This work is from RELENG-1995
    
    Issue-ID: CIMAN-33
    
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>

--- rules.pl
+++ rules.pl
@@ -14,36 +14,30 @@ add_non_author_approval(S1, [label('Non-Author-Code-Review', need(_)) | S1]).
 % Only allow one file to be uploaded, if file is INFO.yaml
 % =============
 submit_filter(In, Out) :-
-    In =.. [submit | Ls],
-	reject_multiple_files_if_INFO_file (Ls, R)
+	In =.. [submit | Ls],
+	reject_multiple_files_if_INFO_file (Ls, R),
+	!,
+	Out =.. [submit | R].
+
+% =============
+% Remove special INFO.yaml label (since not info.yaml)
+% =============
+submit_filter(In, Out) :-
+	In =.. [submit | Ls],
+	remove_verified_info_yaml(Ls, R),
 	Out =.. [submit | R].
 
 reject_multiple_files_if_INFO_file (S1, S2) :-
 	% Ask how many files changed
-	gerrit:commit_stats(F),
-	% CHeck if more than 1 file has changed
-	F > 2,
+	gerrit:commit_stats(F, _, _),
+	% Check if more than 1 file has changed
+	F > 1,
 	% Check if one file name is INFO.yaml
-    gerrit:commit_delta('\\.INFO.yaml$'),
+	gerrit:commit_delta('^INFO.yaml$'),
 	% If anything is false, then reject with Label.
 	S2 = [label('INFO file has to be only file changed', reject(O))|S1].
 
-% =============
-% INFO.yaml requires Jenkins to have given +1 
-% =============
-submit_filter(In, Out) :-
-    In =.. [submit | Ls],
-	if_INFO_file_require_jenkins_plus_1 (Ls, R)
-    Out =.. [submit | R].
+remove_verified_info_yaml([], []).
+remove_verified_info_yaml([label('INFO-Verified', _) | T], R) :- remove_verified_info_yaml(T, R), !.
+remove_verified_info_yaml([H|T], [H|R]) :- remove_verified_info_yaml(T, R).
 
-if_INFO_file_require_jenkins_plus_1 (S1, S2) :-
-	% Ask how many files changed
-	gerrit:commit_stats(F),
-	% Check that only 1 file is changed
-	F = 1,
-	% Check if changed file name is INFO.yaml
-    gerrit:commit_delta('\\.INFO.yaml$'),
-	% Check that Verified is set to +1
-    gerrit:commit_label(label('Verified', 1), R),
-	% If anything is false, then reject with Label.
-	S2 = [label('Jenkins INFO file approval required', reject(O))|S1].
