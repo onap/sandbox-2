3f3682b5522753d00673444da49b4429a4d145b1
Bengt Thuree <bthuree@linuxfoundation.org>
2020-06-17 10:54:59 +1000

    Added Reject Multiple files if INFO
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>
    Change-Id: Id4bdc8d9861b2286225b2dc8df878b974754c35e

--- rules.pl
+++ rules.pl
@@ -11,6 +11,15 @@ add_non_author_approval(S1, S2) :-
 add_non_author_approval(S1, [label('Non-Author-Code-Review', need(_)) | S1]).
 
 % =============
+% Only allow one file to be uploaded, if file is INFO.yaml
+% =============
+submit_filter(In, Out) :-
+	In =.. [submit | Ls],
+	reject_multiple_files_if_INFO_file (Ls, R),
+	!,
+	Out =.. [submit | R].
+
+% =============
 % Remove special INFO.yaml label (since not info.yaml)
 % =============
 submit_filter(In, Out) :-
@@ -21,4 +30,14 @@ submit_filter(In, Out) :-
 remove_verified_info_yaml([], []).
 remove_verified_info_yaml([label('INFO-Verified', _) | T], R) :- remove_verified_info_yaml(T, R), !.
 remove_verified_info_yaml([H|T], [H|R]) :- remove_verified_info_yaml(T, R).
+
+reject_multiple_files_if_INFO_file (S1, S2) :-
+	% Ask how many files changed
+	gerrit:commit_stats(F, _, _),
+	% Check if more than 1 file has changed
+	F > 1,
+	% Check if one file name is INFO.yaml
+	gerrit:commit_delta('^INFO.yaml$'),
+	% If anything is false, then reject with Label.
+	S2 = [label('INFO file has to be only file changed', reject(O))|S1].
 
