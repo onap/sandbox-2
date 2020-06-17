1476f0b1196ed663cf61fb9e0a4f2ac3e75f1fcc
Bengt Thuree <bthuree@linuxfoundation.org>
2020-06-17 10:57:53 +1000

    Removed reject function
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>
    Change-Id: I77a70eca6477549f05d35d881e5459320a5b614b

--- rules.pl
+++ rules.pl
@@ -31,13 +31,5 @@ remove_verified_info_yaml([], []).
 remove_verified_info_yaml([label('INFO-Verified', _) | T], R) :- remove_verified_info_yaml(T, R), !.
 remove_verified_info_yaml([H|T], [H|R]) :- remove_verified_info_yaml(T, R).
 
-reject_multiple_files_if_INFO_file (S1, S2) :-
-	% Ask how many files changed
-	gerrit:commit_stats(F, _, _),
-	% Check if more than 1 file has changed
-	F > 1,
-	% Check if one file name is INFO.yaml
-	gerrit:commit_delta('^INFO.yaml$'),
-	% If anything is false, then reject with Label.
-	S2 = [label('INFO file has to be only file changed', reject(O))|S1].
+reject_multiple_files_if_INFO_file ([]. []).
 
