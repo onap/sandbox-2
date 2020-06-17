20b37dffaeeef90eebbbf30774f71a559c8f585a
Bengt Thuree <bthuree@linuxfoundation.org>
2020-06-17 17:07:12 +1000

    Modified check for INFO.yaml
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>
    Change-Id: Ia07f5ea90f9485e85e4ba5b1497f7316d30051ae

--- rules.pl
+++ rules.pl
@@ -37,7 +37,7 @@ reject_multiple_files_if_INFO_file(S1, S2) :-
 	% Check if more than 1 file has changed
 	F > 1,
 	% Check if one file name is INFO.yaml
-	gerrit:commit_delta('^INFO.yaml$'),
+	gerrit:commit_delta('INFO.yaml'),
 	% If anything is false, then reject with Label.
 	S2 = [label('INFO file has to be only file changed', reject(O))|S1].
 
