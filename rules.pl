12243af5cff2ad3f71a27530fb1c30909215da17
Bengt Thuree <bthuree@linuxfoundation.org>
2020-06-18 19:12:53 +1000

    Again
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>

--- rules.pl
+++ rules.pl
@@ -4,15 +4,19 @@
 submit_filter(In, Out) :-
 	In =.. [submit | Ls],
 	reject_multiple_files_if_INFO_file(Ls, R),
-	!,
 	Out =.. [submit | R].
 
 reject_multiple_files_if_INFO_file(S1, S2) :-
+	% set 0 to be the change owner
+	gerrit:change_owner(O),
 	% Ask how many files changed
 	gerrit:commit_stats(F, _, _),
 	% Check if more than 1 file has changed
 	F > 1,
 	% Check if one file name is INFO.yaml
-	gerrit:commit_delta('INFO.yaml'),
-	% If anything is false, then reject with Label.
-	S2 = [label('INFO-file-has-to-be-only-file-changed', reject(user(ID)))|S1].
+	gerrit:commit_delta('\\INFO.yaml$'),
+	% If you reached here, then reject with Label.
+	S2 = [label('INFO-file-has-to-be-only-file-changed', reject(0))|S1].
+
+reject_multiple_files_if_INFO_file(S1, S2).
+
