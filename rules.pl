2c42317121d8c1232b8dac29a13a3c4b21e96f60
Bengt Thuree <bthuree@linuxfoundation.org>
2020-06-22 14:40:58 +1000

    First proper draft of Only INFO file
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>

--- rules.pl
+++ rules.pl
@@ -42,9 +42,9 @@ ensure_info_file_is_only_file(S1, S2) :-
     % Check if more than 1 file has changed
     ModifiedFiles > 1,
     % Check if one file name is INFO.yaml
-    % gerrit:commit_delta('INFO.yaml'),
+    gerrit:commit_delta('INFO.yaml'),
     % If above two statements are true, give the cut (!) predicate.
-    !,
+    % !,
     % If you reached here, then reject with Label.
     S2 = [label('INFO-Issue', reject(O))|S1].
 
