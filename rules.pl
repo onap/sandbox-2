b505745bcb0fee020b212190601c46534805aaee
Bengt Thuree <bthuree@linuxfoundation.org>
2020-07-09 15:13:10 +1000

    i
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>

--- rules.pl
+++ rules.pl
@@ -88,6 +88,19 @@ if_info_file_require_jenkins_plus_1(S1, S2) :-
     gerrit:change_owner(O),
     % Check if only INFO file is changed.
     is_it_only_INFO_file(),
+    % Check if Verified failed (-1) +1
+    gerrit:commit_label(label('Verified', -1), U),
+    % Confirm correct user gave the -1
+    jenkins_user(U),
+    !,
+    % Jenkins has verified file.
+    S2 = [label('Verified-by-Jenkins', reject(O))|S1].
+
+if_info_file_require_jenkins_plus_1(S1, S2) :-
+    %set O to be the change owner
+    gerrit:change_owner(O),
+    % Check if only INFO file is changed.
+    is_it_only_INFO_file(),
     !,
     S2 = [label('Verified-by-Jenkins', need(O))|S1].
 
