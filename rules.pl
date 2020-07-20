3f15c52c5c4022c6b7d0d3a118da1ade11d12024
Bengt Thuree <bthuree@linuxfoundation.org>
2020-07-20 18:53:13 +1000

    Remove regexp stuff
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>

--- rules.pl
+++ rules.pl
@@ -61,7 +61,6 @@ ensure_info_file_is_only_file(S1, S1).
 jenkins_user(user(459)).   % onap-jobbuilder@jenkins.onap.org
 jenkins_user(user(3)).     % ecomp-jobbuilder@jenkins.openecomp.org
 jenkins_user(user(4937)).  % releng+lf-jobbuilder@linuxfoundation.org
-%jenkins_user(U) :- regex_matches('.*', U).
 
 
 is_it_only_INFO_file() :-
@@ -84,10 +83,6 @@ if_info_file_require_jenkins_plus_1(S1, S2) :-
     gerrit:change_owner(O),
     % Jenkins has verified file.
     S2 = [label('Verified-By-Jenkins', ok(O))|S1].
-    %S2 = [label(U, ok(O))|S1].
-    %S2 = [label(user(459), ok(O))|S1].
-
-
 
 if_info_file_require_jenkins_plus_1(S1, S2) :-
     % Check if only INFO file is changed.
