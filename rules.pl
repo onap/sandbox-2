c70c690aadc05269e775056b0b45c3580a4cf6eb
Bengt Thuree <bthuree@linuxfoundation.org>
2020-07-27 11:39:07 +1000

    Change comment line to be 80 chars max
    Signed-off-by: Bengt Thuree <bthuree@linuxfoundation.org>

--- rules.pl
+++ rules.pl
@@ -1,10 +1,10 @@
 submit_filter(In, Out) :-
     In =.. [submit | Ls],
-    %add the non-owner code review requiremet
+    % add the non-owner code review requiremet
     reject_self_review(Ls, R1),
-    %Reject if multiple files and one is INFO.yaml
+    % Reject if multiple files and one is INFO.yaml
     ensure_info_file_is_only_file(R1, R2),
-    %Reject if not INFO file has been verified by Jenkins
+    % Reject if not INFO file has been verified by Jenkins
     if_info_file_require_jenkins_plus_1(R2, R),
     Out =.. [submit | R].
 
@@ -12,25 +12,28 @@ submit_filter(In, Out) :-
 %filter to require all projects to have a code-reviewer other than the owner
 % =============
 reject_self_review(S1, S2) :-
-    %set O to be the change owner
+    % set O to be the change owner
     gerrit:change_owner(O),
-    %find a +2 code review, if it exists, and set R to be the reviewer
+    % find a +2 code review, if it exists, and set R to be the reviewer
     gerrit:commit_label(label('Code-Review', 2), R),
-    %if there is a +2 review from someone other than the owner, then the filter has no work to do, assign S2 to S1
+    % if there is a +2 review from someone other than the owner, 
+    % then the filter has no work to do, assign S2 to S1
     R \= O, !,
-    %the cut (!) predicate prevents further rules from being consulted
+    % the cut (!) predicate prevents further rules from being consulted
     S2 = S1.
 
 reject_self_review(S1, S2) :-
-    %set O to be the change owner
+    % set O to be the change owner
     gerrit:change_owner(O),
-    %find a +2 code review, if it exists, and set R to be the reviewer - comment sign was missing
+    % find a +2 code review, if it exists, and set R to be the reviewer
     gerrit:commit_label(label('Code-Review', 2), R),
     R = O, !,
-    %if there is not a +2 from someone else (above rule), and there is a +2 from the owner, reject with a self-reviewed label
+    % if there is not a +2 from someone else (above rule), 
+    % and there is a +2 from the owner, reject with a self-reviewed label
     S2 = [label('Self-Reviewed', reject(O))|S1].
 
-% if the above two rules did not make it to the ! predicate, there are not any +2s so let the default rules through unfiltered
+% if the above two rules did not make it to the ! predicate, 
+% there are not any +2s so let the default rules through unfiltered
 reject_self_review(S1, S1).
 
 
@@ -92,7 +95,7 @@ if_info_file_require_jenkins_plus_1(S1, S2) :-
     % Confirm correct user gave the -1
     jenkins_user(U),
     !,
-    %set O to be the change owner
+    % set O to be the change owner
     gerrit:change_owner(O),
     % Jenkins failed verifying file.
     S2 = [label('Verified-By-Jenkins', reject(O))|S1].
@@ -101,7 +104,7 @@ if_info_file_require_jenkins_plus_1(S1, S2) :-
     % Check if only INFO file is changed.
     is_it_only_INFO_file(),
     !,
-    %set O to be the change owner
+    % set O to be the change owner
     gerrit:change_owner(O),
     S2 = [label('Verified-By-Jenkins', need(O))|S1].
 
